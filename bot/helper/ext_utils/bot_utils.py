3c1889123af8eb6b7c4505057a3d248ae8ead110
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-06-22 16:17:42 -0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -57,8 +57,8 @@ def get_readable_file_size(size_in_bytes) -> str:
     if size_in_bytes is None:
         return '0B'
     index = 0
-    while size_in_bytes >= 2024:
-        size_in_bytes /= 2024
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
         index += 1
     try:
         return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
